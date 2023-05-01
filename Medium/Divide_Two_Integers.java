553eabac9166957458a4d5934392a61ce144a2db
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-01 21:53:43 +0530

    question no 29

---
+++ Medium/Divide_Two_Integers.java
@@ -0,0 +1,8 @@
+//Question No: 29
+package Medium;
+
+public class Divide_Two_Integers {
+    public int divide(int dividend, int divisor) {
+        return (int)(dividend/(double)divisor);
+    }
+}
